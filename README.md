248c8a05b0db2fdc3ae42692bf48d28f66f79ce7
ip2location <support@ip2location.com>
2014-12-12 14:50:37 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+Instructions
+============
+
+1. Install **Geo::IP2Location** libraries from CPAN.
+2. Upload **ip2location.pm** to `/usr/share/awstats/plugins`.
+3. Open `/etc/awstats/awstats.conf` and insert following line:
+
+        LoadPlugin="ip2location /usr/share/IP2Location/IP2LOCATION-LITE-DB1.BIN"
+
+    **Note: ** Make sure the path to IP2Location database file is correct.
+
+4. Disable any Maxmind GeoIP plugin as AWStats hook can only access by one plugin per time.
+
+5. View the information by accessing "Countries" or "Hosts" from the left menu.
+
+6. It's recommended to use IP2Location DB3 which included country, region, and city information.
+   Download free version from http://lite.ip2location.com or commercial version from
+   http://www.ip2location.com.
---
+++ ip2location.pm
@@ -0,0 +1,127 @@
+#!/usr/bin/perl
+#-----------------------------------------------------------------------------
+# IP2Location AWStats plugin
+# This plugin allow you to get AWStats country report with country name.
+#-----------------------------------------------------------------------------
+# Perl Required Modules: Geo::IP2Location
+#-----------------------------------------------------------------------------
+
+# <-----
+# ENTER HERE THE USE COMMAND FOR ALL REQUIRED PERL MODULES
+push @INC, "${DIR}/plugins";
+if (!eval ('require "Geo/IP2Location.pm";')) { return $@?"Error: $@":"Error: Need Perl module Geo::IP2Location"; }
+# ----->
+#use strict;
+no strict "refs";
+
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN VARIABLES
+#-----------------------------------------------------------------------------
+# <-----
+# ENTER HERE THE MINIMUM AWSTATS VERSION REQUIRED BY YOUR PLUGIN
+# AND THE NAME OF ALL FUNCTIONS THE PLUGIN MANAGE.
+my $PluginNeedAWStatsVersion="5.5";
+my $PluginHooksFunctions="ShowInfoHost";
+# ----->
+
+# <-----
+# IF YOUR PLUGIN NEED GLOBAL VARIABLES, THEY MUST BE DECLARED HERE.
+use vars qw/
+%TmpDomainLookup
+$obj
+/;
+# ----->
+
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: Init_pluginname
+#-----------------------------------------------------------------------------
+sub Init_ip2location {
+	my $InitParams=shift;
+	my $checkversion=&Check_Plugin_Version($PluginNeedAWStatsVersion);
+
+	# <-----
+	# ENTER HERE CODE TO DO INIT PLUGIN ACTIONS
+	debug(" Plugin ip2location: InitParams=$InitParams",1);
+	my ($datafile,$override)=split(/\s+/,$InitParams,2);
+
+	%TmpDomainLookup=();
+	$obj = Geo::IP2Location->open($datafile);
+	# ----->
+
+	return ($checkversion?$checkversion:"$PluginHooksFunctions");
+}
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: GetCountryCodeByName_pluginname
+# UNIQUE: YES (Only one plugin using this function can be loaded)
+# GetCountryCodeByName is called to translate a host name into a country name.
+#-----------------------------------------------------------------------------
+sub GetCountryCodeByName_ip2location {
+    my $param="$_[0]";
+	# <-----
+	my $res=$TmpDomainLookup{$param}||'';
+	if (! $res) {
+		($res,undef)=$obj->LookUp($param);
+		if ($res !~ /\w\w/) { $res='ip'; }
+		else { $res=lc($res); }
+		$TmpDomainLookup{$param}=$res;
+		if ($Debug) { debug("  Plugin ip2location: GetCountryCodeByName for $param: $res",5); }
+	}
+	elsif ($Debug) { debug("  Plugin ip2location: GetCountryCodeByName for $param: Already resolved to $res",5); }
+	# ----->
+	return $res;
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: ShowInfoHost_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to add additionnal columns to the Hosts report.
+# This function is called when building rows of the report (One call for each
+# row). So it allows you to add a column in report, for example with code :
+#   print "<TD>This is a new cell for $param</TD>";
+# Parameters: Host name or ip
+#-----------------------------------------------------------------------------
+sub ShowInfoHost_ip2location {
+    my $param="$_[0]";
+	# <-----
+	if ($param eq '__title__') {
+    	my $NewLinkParams=${QueryString};
+    	$NewLinkParams =~ s/(^|&)update(=\w*|$)//i;
+    	$NewLinkParams =~ s/(^|&)output(=\w*|$)//i;
+    	$NewLinkParams =~ s/(^|&)staticlinks(=\w*|$)//i;
+    	$NewLinkParams =~ s/(^|&)framename=[^&]*//i;
+    	my $NewLinkTarget='';
+    	if ($DetailedReportsOnNewWindows) { $NewLinkTarget=" target=\"awstatsbis\""; }
+    	if (($FrameName eq 'mainleft' || $FrameName eq 'mainright') && $DetailedReportsOnNewWindows < 2) {
+    		$NewLinkParams.="&framename=mainright";
+    		$NewLinkTarget=" target=\"mainright\"";
+    	}
+    	$NewLinkParams =~ tr/&/&/s; $NewLinkParams =~ s/^&//; $NewLinkParams =~ s/&$//;
+    	if ($NewLinkParams) { $NewLinkParams="${NewLinkParams}&"; }
+
+		print "<th width=\"80\">Country</th>";
+		print "<th width=\"150\">Region</th>";
+		print "<th width=\"150\">City</th>";
+	}
+	elsif ($param) {
+		my $country_long = $obj->get_country_long($param);
+		my $region  = $obj->get_region($param);
+		my $city = $obj->get_city($param);
+
+		print "<td><span style=\"color: #$color_other\">$country_long</span></td>";
+		print "<td><span style=\"color: #$color_other\">$region</span></td>";
+		print "<td><span style=\"color: #$color_other\">$city</span></td>";
+	}
+	else {
+		print "<td>&nbsp;</td>";
+	}
+	return 1;
+	# ----->
+}
+
+1;
